47efe150733b878763fc5c71105c73657d079118
rfyiamcool <rfyiamcool@163.com>
2019-04-17 20:52:19 +0800

    add raft lib

---
+++ consensus/init.sh
@@ -0,0 +1,15 @@
+# rm -rf *
+cp -r ~/go/src/go.etcd.io/etcd/wal .
+cp -r ~/go/src/github.com/rfyiamcool/raft_kvs/consensus/snap .
+cp -r ~/go/src/github.com/rfyiamcool/raft_kvs/consensus/v2stats  .
+cp -r ~/go/src/go.etcd.io/etcd/etcdserver/api/rafthttp .
+cp -r ~/go/src/go.etcd.io/etcd/raft .
+
+# github.com/rfyiamcool/raft_kvs/consensus/raft/raftpb
+# github.com/rfyiamcool/raft_kvs/consensus/raft/raftpb
+
+# github.com/rfyiamcool/raft_kvs/consensus/snap
+# github.com/rfyiamcool/raft_kvs/consensus/snap
+
+# github.com/rfyiamcool/raft_kvs/consensus/v2stats
+# github.com/rfyiamcool/raft_kvs/consensus/v2stats
---
+++ log/logger_test.go
@@ -0,0 +1,10 @@
+package log
+
+import (
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	// Log.Info("state is ok")
+	// Log.Infof("state is %s, err: %v", "ok", nil)
+}
